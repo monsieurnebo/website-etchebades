7057ef55148aac8e7c1578002fff7c2971259d92
Cyril Bonnet <ds.cyril@gmail.com>
2021-09-30 13:12:07 +0200

    feat: Add legal page WIP

---
+++ components/Legal/Legal.js
@@ -0,0 +1,21 @@
+import styles from "./legal.module.scss";
+
+export default function Legal() {
+  const { title, subtitle, text, link } = styles;
+  return (
+    <div>
+      <h1 className={title}>Mentions légales</h1>
+      <h2 className={subtitle}>Hébergement</h2>
+      <ul className={text}>
+        <li>Netlify, Inc.</li>
+        <li>2325 3rd Street, Suite 215, San Francisco, California 94107</li>
+        <li><a className={link} href="mailto:support@netlify.com">support@netlify.com</a></li>
+      </ul>
+      <p className={text}>
+        Ce site a été développé par : <a className={link} href="https://cyrilbonnet.fr/" target="_blank" rel="noreferrer">Cyril BONNET, Webmaster Freelance Strasbourg, création site internet & SEO</a>.
+      </p>
+      <h2 className={subtitle}>Droits d'auteur</h2>
+      <p className={text}>Photographie de Philippe Etchebest par © M6</p>
+    </div>
+  );
+}
---
+++ pages/mentions-legales.js
@@ -0,0 +1,14 @@
+import Layout from "../components/Layout/Layout";
+import Legal from "../components/Legal/Legal";
+
+export default function MentionsLegales() {
+
+  return (
+    <Layout>
+      <Legal />
+    </Layout>
+  );
+}
+
+
+
