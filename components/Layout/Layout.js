39722fa6b47f5d1b5e5b0099dab2833639365117
Cyril Bonnet <ds.cyril@gmail.com>
2021-09-30 13:11:44 +0200

    feat: Add home link to layout title

--- components/Layout/Layout.js
+++ components/Layout/Layout.js
@@ -1,11 +1,16 @@
+import Link from "next/link";
 import Footer from "../Footer/Footer";
 import styles from "./layout.module.scss";
 
 export default function Layout({ children }) {
   return (
     <div className={styles.layout}>
-      <h1 className={styles.title}>
-        Les Etchebades
+      <h1>
+        <Link passHref href="/">
+          <a className={styles.title__link} title="Retourner à la page d'accueil">
+            Les Etchebades
+          </a>
+        </Link>
       </h1>
       {children}
       <Footer />
