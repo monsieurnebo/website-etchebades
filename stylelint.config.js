117f6ca757139f92b96723c02938217620e25996
Cyril Bonnet <ds.cyril@gmail.com>
2021-12-04 19:41:19 +0100

    chore: Update dependencies
    And fix new lint rules.
    
    Closes #7

--- stylelint.config.js
+++ stylelint.config.js
@@ -1,6 +1,7 @@
 module.exports = {
   extends : [
-    "stylelint-config-standard"
+    "stylelint-config-standard",
+    "stylelint-config-standard-scss",
   ],
   plugins : ["stylelint-scss"],
   // add your custom config here
@@ -8,6 +9,9 @@ module.exports = {
   rules : {
     // Enable SCSS rules https://github.com/kristerkari/stylelint-scss
     "at-rule-no-unknown" : null,
-    "scss/at-rule-no-unknown" : true
+    "scss/at-rule-no-unknown" : true,
+    // Allow BEM class pattern (see https://gist.github.com/Potherca/f2a65491e63338659c3a0d2b07eee382 )
+    "selector-class-pattern" : "^\.[a-z]([a-z0-9-]+)?(__([a-z0-9]+-?)+)?(--([a-z0-9]+-?)+){0,2}$",
+    "scss/double-slash-comment-empty-line-before" : null
   }
 };
