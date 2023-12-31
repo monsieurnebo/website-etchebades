f1e47fc9fd0a5efd225f95f24f58d0f984c191bc
monsieurnebo <ds.cyril@gmail.com>
2023-12-31 09:29:42 +0100

    chore: Add Vercel Analytics
    Closes #17

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,3 +1,4 @@
+import { Analytics } from '@vercel/analytics/react';
 import type { Metadata } from 'next'
 import { Noto_Serif } from 'next/font/google'
 
@@ -36,6 +37,7 @@ export default function RootLayout({
             </Link>
           </h1>
           {children}
+          <Analytics />
           <Footer />
         </div>
       </body>
